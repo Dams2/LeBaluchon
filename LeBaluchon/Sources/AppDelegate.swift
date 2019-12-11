02405c9d9b89e410123bd46f75dd7e09be74306f
Dams2 <damien-rojo@hotmail.fr>
2019-12-11 18:31:14 +0100

    First commit

---
+++ LeBaluchon/Sources/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  LeBaluchon
+//
+//  Created by Damien Rojo on 11.12.19.
+//  Copyright © 2019 Damien Rojo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
